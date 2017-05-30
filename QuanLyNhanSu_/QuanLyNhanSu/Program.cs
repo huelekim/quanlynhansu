3987a058c93fefd8d273bf03bb85d52ad3dee2e2
huelekim <hue1996mta@gmail.com>
2017-05-30 10:59:19 +0700

    Tạo form đăng nhập_Thiết kế

--- QuanLyNhanSu_/QuanLyNhanSu/Program.cs
+++ QuanLyNhanSu_/QuanLyNhanSu/Program.cs
@@ -17,7 +17,7 @@ namespace QuanLyNhanSu
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmNhanVien());
+            Application.Run(new frmDangNhap());
         }
     }
 }
